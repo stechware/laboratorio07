e2898299319a8ec4443d01e9408c95f47b420ce9
Juan Solar <stchware2023@gmail.com>
2025-02-24 07:32:22 -0500

    cambiando Mensaje

--- laboratorio07/src/main/java/com/idat/laboratorio07/Laboratorio07.java
+++ laboratorio07/src/main/java/com/idat/laboratorio07/Laboratorio07.java
@@ -11,6 +11,6 @@ package com.idat.laboratorio07;
 public class Laboratorio07 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola desde Github. Aprendiendo PULL");
     }
 }
